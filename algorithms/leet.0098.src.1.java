6290ba1eedd5bb7cbb8b8597f94d82931d42f2c7
Alfred Huang <alfred.h@163.com>
2015-03-11 01:50:59 +0800

    Create leet.0098.src.1.java

---
+++ algorithms/leet.0098.src.1.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    Integer low = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        if(!isValidBST(root.left)) return false;
+        if(low != null && low >= root.val) return false;
+        low = root.val;
+        return isValidBST(root.right);
+    }
+}
