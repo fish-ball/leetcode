38289179231ab306d2516e47d770ad1b6db2ec98
Alfred Huang <57082212@qq.com>
2021-02-08 17:46:26 +0800

    Update leet.0028.src.1.py

--- algorithms/leet.0028.src.1.py
+++ algorithms/leet.0028.src.1.py
@@ -1,14 +1,13 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
         n = len(haystack)
         m = len(needle)
         for i in range(n-m+1):
-            fail = False
+            yes = True
             for j in range(m):
                 if needle[j] != haystack[i+j]:
-                    fail = True
-            if not fail:
+                    yes = False
+                    break
+            if yes:
                 return i
         return -1
