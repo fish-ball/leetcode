784571cc655b6a5a990763d1aaa456038a70e01a
Alfred Huang <57082212@qq.com>
2021-05-14 21:13:01 +0800

    Create offer.0006.src.1.py

---
+++ algorithms/offer.0006.src.1.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if not head:
+            return []
+        ans = self.reversePrint(head.next)
+        ans.append(head.val)
+        return ans
