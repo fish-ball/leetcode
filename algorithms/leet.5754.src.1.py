dc3f9193e86bd92e020e40ed756e889fa4fd2118
Alfred Huang <57082212@qq.com>
2021-05-30 01:10:39 +0800

    Create leet.5754.src.1.py
    简单模拟，暴力，O(n)

---
+++ algorithms/leet.5754.src.1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        for i in range(n-1):
+            if len(set(s[i:i+3])) == 3:
+                ans += 1
+        return ans
