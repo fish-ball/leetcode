15b0d83aa0a7e15b8baec8b2e46bb2f0b13256a1
Alfred Huang <57082212@qq.com>
2021-09-10 11:26:27 +0800

    Create leet.0387.src.1.py
    简单题，先用 Counter 计数，再遍历一次 O(nlogn)

---
+++ algorithms/leet.0387.src.1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        ct = Counter(s)
+        for i, c in enumerate(s):
+            if ct[c] == 1:
+                return i
+        return -1
+        
