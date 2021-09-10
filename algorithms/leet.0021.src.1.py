08784222f5756b18df19f65c5f536893564431e8
Alfred Huang <57082212@qq.com>
2021-09-10 11:39:07 +0800

    Create leet.0021.src.1.py

---
+++ algorithms/leet.0021.src.1.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = ListNode()
+        p = root
+        while l1 or l2:
+            if not l2 or l1 and l2 and l1.val <= l2.val:
+                p.next, l1 = l1, l1.next
+            else:
+                p.next, l2 = l2, l2.next
+            p = p.next
+        return root.next
