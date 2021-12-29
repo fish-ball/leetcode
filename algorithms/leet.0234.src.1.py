80db274600984592c38161cd2471dbfea6ef1afd
Alfred Huang <57082212@qq.com>
2021-12-29 22:25:14 +0800

    Create leet.0234.src.1.py
    回文链表，直接转存到列表判断，秒杀。

---
+++ algorithms/leet.0234.src.1.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        a = []
+        p = head
+        while p:
+            a.append(p.val)
+            p = p.next
+        return a == a[::-1]
